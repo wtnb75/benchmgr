3d435663c3c6c8a91dd90ffc0f67e81856bbb481
Watanabe Takashi <wtnb75@gmail.com>
2026-01-29 21:42:06 +0900

    formatter (#5)

--- mongoose/main.c
+++ mongoose/main.c
@@ -1,9 +1,9 @@
-#include "mongoose.h"   // To build, run: cc main.c mongoose.c
+#include "mongoose.h"  // To build, run: cc main.c mongoose.c
 
 // HTTP server event handler function
-void ev_handler(struct mg_connection *c, int ev, void *ev_data) {
+void ev_handler(struct mg_connection* c, int ev, void* ev_data) {
   if (ev == MG_EV_HTTP_MSG) {
-    struct mg_http_message *hm = (struct mg_http_message *) ev_data;
+    struct mg_http_message* hm = (struct mg_http_message*)ev_data;
     mg_http_reply(c, 200, "", "");
   }
 }
@@ -11,9 +11,9 @@ void ev_handler(struct mg_connection *c, int ev, void *ev_data) {
 int main(void) {
   struct mg_mgr mgr;  // Declare event manager
   mg_log_set(MG_LL_ERROR);
-  mg_mgr_init(&mgr);  // Initialise event manager
+  mg_mgr_init(&mgr);                                            // Initialise event manager
   mg_http_listen(&mgr, "http://0.0.0.0:80", ev_handler, NULL);  // Setup listener
-  for (;;) {          // Run an infinite event loop
+  for (;;) {                                                    // Run an infinite event loop
     mg_mgr_poll(&mgr, 1000);
   }
   return 0;
